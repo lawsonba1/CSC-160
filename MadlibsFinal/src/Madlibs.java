ac6c8e5c6f152aebf519ab92003bff11ecf8d274
Werewolf <lawsonba1@s.dcsdk12.org>
2021-03-01 13:12:50 -0700

    inital comit

--- MadlibsFinal/src/Madlibs.java
+++ MadlibsFinal/src/Madlibs.java
@@ -3,9 +3,13 @@ import java.util.Scanner;  // Import the Scanner class
 class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
 
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
+
+        System.out.println("Please enter a noun.");
+        String noun1 = myObj.nextLine();  // Read user input
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj1 = myObj.nextLine();
+
+        System.out.println();
     }
 }
