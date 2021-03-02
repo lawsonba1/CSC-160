92608ee2e3df5b0bb87cc8e492302fc9082d0d4e
lawsonba1 <lawsonba1@s.dcsdk12.org>
2021-03-02 12:54:36 -0700

    15 questions

--- MadlibsFinal/src/Madlibs.java
+++ MadlibsFinal/src/Madlibs.java
@@ -4,12 +4,38 @@ class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-
+        // 15 questions
         System.out.println("Please enter a noun.");
         String noun1 = myObj.nextLine();  // Read user input
         System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
         String adj1 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj2 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj3 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj4 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj5 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj6 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj7 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj8 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj9 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj10 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj11 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj12 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj13 = myObj.nextLine();
+        System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
+        String adj14 = myObj.nextLine();
 
-        System.out.println("You are a " +adj1 +noun1);
+        System.out.println("You are a " +adj1 +" "+adj2 +" "+adj3 +" "+adj4 +" "+adj5 +" "+adj6 +" "+adj7 +" "+adj8 +" "+adj9 +" "+adj10 +" "+adj11 +" "+adj12 +" "+adj13 +" "+adj14 +" " +noun1 +". How silly!");
     }
 }
