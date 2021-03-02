57cf520db285e71d99f1b08900a954843987c9ba
lawsonba1 <lawsonba1@s.dcsdk12.org>
2021-03-02 13:13:52 -0700

    15 questions/part 2

--- MadlibsFinal/src/Madlibs.java
+++ MadlibsFinal/src/Madlibs.java
@@ -4,7 +4,11 @@ class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-        // 15 questions
+        // 15 questions nice job brady
+        //pog champ?
+        //wow sooooo creative
+        //Thanks!!
+        //that was sarcasm
         System.out.println("Please enter a noun.");
         String noun1 = myObj.nextLine();  // Read user input
         System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
@@ -36,6 +40,6 @@ class Main {
         System.out.println("Thanks! Now, please enter a adjective.");  // Output user input
         String adj14 = myObj.nextLine();
 
-        System.out.println("You are a " +adj1 +" "+adj2 +" "+adj3 +" "+adj4 +" "+adj5 +" "+adj6 +" "+adj7 +" "+adj8 +" "+adj9 +" "+adj10 +" "+adj11 +" "+adj12 +" "+adj13 +" "+adj14 +" " +noun1 +". How silly!");
+        System.out.println("You are a " +adj1 +" "+adj2 +" "+adj3 +" "+adj4 +" "+adj5 +" "+adj6 +" "+adj7 +" "+adj8 +" "+adj9 +" "+adj10 +" "+adj11 +" "+adj12 +" "+adj13 +" "+adj14 +" " +noun1 +". Very discriptive.");
     }
 }
