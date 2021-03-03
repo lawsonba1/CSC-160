73e629c2143f8f571130726cd6abed58897fb3f9
Werewolf <lawsonba1@s.dcsdk12.org>
2021-03-03 16:24:16 -0700

    8ball pt4

--- Magic8Ball/src/Magic8Ball.java
+++ Magic8Ball/src/Magic8Ball.java
@@ -6,24 +6,16 @@ public class Magic8Ball {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("What's your question?");
+        String question = reader.nextLine();
 
-        //  String[] possibleAnswers = {"As I see it yes.", "Ask again later.", "Better not tell you now.",
-        //       "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.",
-        //        "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.",
-        //       "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
-        //       "Yes.", "Yes – definitely.", "You may rely on it."};
-//
-        //       while(true){
-        //       System.out.println("Ask me a question mortal.");
+        randomNumber();
+    }
 
-        //       Random randNum = new Random(possibleAnswers.length);
-
-        //        System.out.println(randNum);
-        //   }
-        // }
-        boolean ploggers = true;
-        int randomNumber;
-        String[] posibleOutcomes = {"It is certain.",
+    public static void randomNumber() {
+        boolean ploggers =false;
+        Scanner reader = new Scanner(System.in);
+        String[] possibleOutcomes = {"It is certain.",
                 "It is decidedly so.",
                 "Without a doubt.",
                 "Yes – definitely.",
@@ -43,29 +35,36 @@ public class Magic8Ball {
                 " My sources say no.",
                 " Outlook not so good.",
                 "Very doubtful"};
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(20);
+        String option = possibleOutcomes[randomNumber];
+        System.out.println(option);
 
-        while (ploggers) {
-            System.out.println("Ask me a question mortal.");
-            String question = reader.nextLine();
-
-
-            Random rand = new Random();
-            randomNumber = rand.nextInt(20);
-            String option = posibleOutcomes[randomNumber];
-            System.out.println(option);
-
-            System.out.print("Do you have another question?");
-            String yesOrNo = reader.nextLine();
-            if (yesOrNo=="no"){
-                ploggers=false;
-            }
-
-
-
-
-
+        System.out.println("Wanna ask another question?");
+        String yesOrNo = reader.nextLine();
+        if (yesOrNo=="no"){
+            ploggers=true;
         }
+        while (ploggers) {
+        no();
+        }
+        main();
+
+
+    }
+
+    public static void no() {
+        System.out.println("Thanks for playing!");
+
+    }
+    public static void main(){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("What's your question?");
+        String question = reader.nextLine();
+        randomNumber();
     }
 }
+
 
 
